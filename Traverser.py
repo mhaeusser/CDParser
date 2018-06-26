22b82b74b2fc24db7fa13d314d49c891e29ebdbb
Matthias Häußer <mhaeusser@web.de>
2018-06-26 09:39:18 +0200

    Write into file.

--- Traverser.py
+++ Traverser.py
@@ -6,8 +6,10 @@ class Traverser:
   def __init__(self):
     pass
 
-  def traverse(self, root_dir):
+  def traverse(self, root_dir, out_filename):
     print('Starting.')
+    out_file = open(out_filename, "w", encoding="utf8")
+
     special_folders = self.get_special_folders()
 
     artist_dirs = os.listdir(root_dir)
@@ -17,13 +19,13 @@ class Traverser:
       
         artist_dir = os.path.join(root_dir, folder)
         if (os.path.isdir(artist_dir)):
-          self.print_artist_folder(root_dir, artist_dir)
+          self.print_artist_folder(artist_dir, folder, out_file)
+    print('See ' + out_filename)
 
-  def print_artist_folder(self, root_dir, artist_dir):
+  def print_artist_folder(self, artist_dir, artist_name, out_file):
     album_folders = os.listdir(artist_dir)
-    print(artist_dir)
     for album_folder in album_folders:
-      print("\t\t" + album_folder)
+      out_file.write(artist_name + "\t" + album_folder + "\n")
 
   def get_special_folders(self):
     return ['[Live Mixes]', '[Other]', '[Spoken Word]', '__Eval', '__New (must have tags)', '_Einzelne', '_Various Artists']
--- TraverserRunner.py
+++ TraverserRunner.py
@@ -3,4 +3,4 @@ class TraverserRunner:
   # Runs Traverser with path hard-coded here.
   if __name__ == "__main__":
     traverser = Traverser()
-    traverser.traverse('D:/SkaMP3')
+    traverser.traverse('D:/SkaMP3', 'C:/temp/CDs.csv')
