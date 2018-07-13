57e47f91692e25fe7e5452a62cb3e8a82142369d
Matthias Häußer <matthias.haeusser@t-systems.com>
2018-07-13 16:16:58 +0200

    Simplified.

--- TraverserRunner.py
+++ TraverserRunner.py
@@ -3,4 +3,7 @@ class TraverserRunner:
   # Runs Traverser with path hard-coded here.
   if __name__ == "__main__":
     traverser = Traverser()
-    traverser.traverse('D:/SkaMP3', 'C:/temp/CDs.csv')
+    root_dir = 'C:/Users/mathaeus/Music/_SKA'
+    #root_dir = 'D:/SkaMP3'
+    out_file = 'C:/temp/CDs.csv'
+    traverser.traverse(root_dir, out_file)
