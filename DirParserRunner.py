36ec26d2d8a0f96ecb605d0e4e03da43daa2df24
Matthias Häußer <matthias.haeusser@t-systems.com>
2018-07-13 16:14:43 +0200

    Special treatment for Various Artists.

--- DirParserRunner.py
+++ DirParserRunner.py
@@ -1,10 +1,16 @@
-class TraverserRunner:
+import os
+
+class DirParserRunner:
 
   # Runs DirParser with path hard-coded here.
   if __name__ == "__main__":
     dirParser = DirParser()
     folders_to_ignore = ['[Live Mixes]', '[Other]', '[Spoken Word]', '__Eval', '__New (must have tags)', '_Einzelne', '_Various Artists']
-    dict = dirParser.traverse('C:/Users/mathaeus/Music/_SKA', folders_to_ignore)
+    root_dir = 'C:/Users/mathaeus/Music/_SKA'
+    dict = dirParser.traverse(root_dir, folders_to_ignore)
+
+    va_dir = os.path.join(root_dir, '_Various Artists')
+    dict[va_dir] = os.listdir(va_dir)
 
     text = dirParser.format_dict(dict)
     print(text)
